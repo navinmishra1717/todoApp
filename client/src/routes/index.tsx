b6f56397226cffbcf2ab42ad8e8ec7ecf090f5c3
Navin Mishra <navinmishra1717@gmail.com>
2024-05-17 17:26:47 +0545

    add users page

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -4,12 +4,17 @@ import Loadable from '../components/Loadable';
 
 // todos page routing
 const TodoPage = Loadable(lazy(() => import('../views/Todos')));
+const UserListPage = Loadable(lazy(() => import('../views/Users')));
 
 export default function Routes() {
     return useRoutes([
         {
             path: '/',
             element: <TodoPage />
+        },
+        {
+            path: '/users',
+            element: <UserListPage />
         }
     ]);
 }
